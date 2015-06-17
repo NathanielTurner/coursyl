6eb94cb46c9918a13ca8e74ba50b8c55b7522087
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-17 00:22:15 -0400

    Fork

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,45 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+window.onload = hideLastRow;
+
+function hideLastRow(){
+  var associations = document.getElementsByClassName("association container");
+  var lastAssociation = associations[ associations.length - 1 ];
+  var rowToHide = lastAssociation.lastElementChild;
+  rowToHide.style.display = "none";
+}
+
+function showLastRow(){
+  var associations = document.getElementsByClassName("association container");
+  var lastAssociation = associations[ associations.length - 1 ];
+  var rowToShow = lastAssociation.lastElementChild;
+  rowToShow.style.display = "block";
+}
+
+function disableOnClick(){
+  document.getElementById("submit_form").disabled = true;
+}
+
+function hideSection(section){
+  var rowToHide = section.parentElement.parentElement.parentElement;
+  rowToHide.style.display = "none";
+  //hides the columns but refreshing the page brings them back, no delete happens.
+}
+
+// WOOOOOW I googled it, got a box on the first webpage with this function inside
+// just stuck it in and it worked, plug and play to the extreame!!!
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
